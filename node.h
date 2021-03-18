54ca40c4e3ba4b0f8845081b3a041856f162174d
ptrandev <ptrandev@bu.edu>
2021-03-18 03:49:48 +0000

    comment for addNode method

--- node.h
+++ node.h
@@ -2,6 +2,6 @@
 #define FUNCTIONS_NODE_H
 
 typedef struct node node;
-struct node* createNode(int value);
+struct node* createNode(int value); // creates a new node with the input value
 
 #endif // FUNCTIONS_NODE_H
