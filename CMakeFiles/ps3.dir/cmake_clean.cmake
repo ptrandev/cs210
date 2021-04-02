c584380fc4436f529ee51e173f190a38a646c91f
ptrandev <ptrandev@bu.edu>
2021-04-02 00:22:04 +0000

    initial commit

---
+++ CMakeFiles/ps3.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ps3.dir/main.c.o"
+  "ps3.pdb"
+  "ps3"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/ps3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
